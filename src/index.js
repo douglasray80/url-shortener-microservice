330ee78f7424bc9a6720f6cb04e6e2f668095c32
Douglas Ray <dpr980@gmail.com>
2019-04-22 15:19:39 +0100

    added mongoose, dotenv-cli packages, initialized mongoose

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-const dns = require('dns')
-const bodyParser = require('body-parser')
 const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const cors = require('cors')
+const dns = require('dns')
 
 const app = express()
 
@@ -35,6 +36,18 @@ app.post('/api/shorturl/new', (req, res) => {
 	res.json({ original_url: '', short_url: '' })
 })
 
-const listener = app.listen(process.env.PORT || 4000, () => {
-	console.log('Your app is listening on port ' + listener.address().port)
-})
+mongoose.connect(
+	process.env.MONGO_URI,
+	{ useNewUrlParser: true },
+	(err, db) => {
+		if (err) {
+			console.log('Database error: ' + err)
+		} else {
+			console.log('Successful database connection')
+
+			app.listen(process.env.PORT || 4000, () => {
+				console.log('Your app is listening on port 4000')
+			})
+		}
+	}
+)
